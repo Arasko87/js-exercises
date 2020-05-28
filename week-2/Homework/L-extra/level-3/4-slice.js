66a9731c07cb487249f47f54e428e0c15b41a85b
Aras Sulaiman <arasko87@gmail.com>
2020-05-28 21:26:50 +0100

    week-2/Homework/L-extra/level-3/4-slice.js DONE

--- week-2/Homework/L-extra/level-3/4-slice.js
+++ week-2/Homework/L-extra/level-3/4-slice.js
@@ -5,7 +5,8 @@
 // - insert the replacement value at the provided index
 
 function replace(arr, index, value) {
-  return  ; 
+  
+  return newArr = arr .slice(0, index).concat(value).concat(arr.slice(index + 1));
   
   // complete this statement
 }
