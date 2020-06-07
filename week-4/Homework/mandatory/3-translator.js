a14fec6934158786bc1375d5b8a09fa535e3319e
Aras Sulaiman <arasko87@gmail.com>
2020-06-07 12:22:37 +0100

    Done

--- week-4/Homework/mandatory/3-translator.js
+++ week-4/Homework/mandatory/3-translator.js
@@ -27,35 +27,16 @@ It should default to English if the language is not in your object of languages,
 or in the event of an invalid input.
 */
 
-/*
-// we return all the objects to lower Case then 
+
 function greet(language) {
-  //console.log(" second ",  languages[language.toLowerCase()]);
-//  console.log(" first", languages[language]);
-  
-const key = language.toLowerCase();
-
-  if (Object.keys(languages).includes(key))
-  {
-    return languages[key];}
-    else{
- //     console.log("Hiiiiiiii");
-
-    return "please make sure you entered the correct language, or check your spelling and Caps Lock";
-    }
-  }
-
-console.log(greet("arabic"));
-*/
-function greet(language) {
-  const key = language.toLowerCase();
-  if (languages[key]) {
-    return languages[key];
+  const lan = language.toLowerCase();
+  if (languages[lan]) {
+    return languages[lan];
   } else {
     return "No language available";
   }
 }
-console.log(greet("KUrdish"));
+console.log(greet("KuRdisH"));
 /*
 Test your function works correctly calling it inside a console.log(), for each one of these cases:
 
@@ -67,4 +48,22 @@ For example: console.log(greet("irish"));
 3. pass it a language that doesn't exist in the object
 4. pass it an invalid string (something that is not even a language)
 
+*/
+
+
+
+ /*
+function greet(language) {
+  
+const key = language.toLowerCase();
+
+  if (Object.keys(languages).includes(key))
+  {
+    return languages[key];}
+    else{
+    return "please make sure you entered the correct language.";
+    }
+  }
+
+console.log(greet("arabic"));
 */
