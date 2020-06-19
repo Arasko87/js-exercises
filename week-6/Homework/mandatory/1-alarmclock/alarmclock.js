e3f7cd602145fc34a7a5a572e27f7a64f89f9f0a
Aras Sulaiman <arasko87@gmail.com>
2020-06-19 20:37:38 +0100

    Alarm.js finished

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,9 +1,34 @@
-function setAlarm() {}
+ 
+function setAlarm() {
+  
+  let inputValue = document.querySelector("#alarmSet").value;
+
+  let enteredValue = document.querySelector("#timeRemaining");
+  
+      timer(inputValue,enteredValue);
+
+      var myVar = setInterval(function(){
+
+      inputValue = inputValue -1 ;
+      timer(inputValue,enteredValue);
+
+      if (inputValue === 0 ){
+        playAlarm();
+        clearInterval(myVar);
+      }
+
+    }, 1000);
+}
+function timer(time,any){
+
+any.textContent = "Time Remaining 00: " + time ;
+
+}
 
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
-
+audio.volume = 0.1;
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
