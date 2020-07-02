1a32ae32e45db663cab1d1a13f55512e03bf7cf8
Aras Sulaiman <arasko87@gmail.com>
2020-07-02 15:22:03 +0100

    Correction of in class ex done

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -14,21 +14,12 @@ Write JavaScript below that logs:
     4. all the "p" elements of contained inside  the .primary-content element node
     --> should log a list of nodes with a length of 3
 */
-console.log(document.querySelectorAll("p"));
 
-<<<<<<< Updated upstream
 document.querySelectorAll("P");
 document.querySelector(".site-header");
 document.querySelector("#jumbotron-text");
 document.querySelectorAll(".primary-content  p");
 
-=======
-console.log(document.querySelector(".site-header"))
-
-console.log(document.querySelector("#jumbotron-text"))
-
-console.log(document.querySelectorAll((".primary-content p")))
->>>>>>> Stashed changes
 
 /*
 Task 2
@@ -36,22 +27,13 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
-<<<<<<< Updated upstream
-let myButton = document.querySelector("#alertBtn");
-myButton.addEventListener("click",alertThanks );
 
-function alertThanks(){
-    alert("Thank for visiting Bikes for Refugee!")
-}
-
-
-=======
 let alertButton = document.querySelector("#alertBtn");
 alertButton.addEventListener("click", vote)
 function vote(){
     alert ("Thanks for visiting Bikes for Refugees!");
 }
->>>>>>> Stashed changes
+
 /*
 Task 3
 =======
@@ -59,23 +41,12 @@ Task 3
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
 let colorChange = document.querySelector("#bgrChangeBtn");
-colorChange.addEventListener("click",newColor)
-function newColor(){
-
-<<<<<<< Updated upstream
-let changeBckColor = document.querySelector("#bgrChangeBtn");
-changeBckColor.addEventListener("click",bkColor);
+colorChange.addEventListener("click",bkColor);
 
 function bkColor (){
 
 document.body.style.backgroundColor="pink";
-
 }
-
-=======
-    bgrChangeBtn.style.backgroundColor = "red";
-}
->>>>>>> Stashed changes
 
 /*
 Task 4
@@ -105,7 +76,7 @@ Task 5
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
 
-<<<<<<< Updated upstream
+
 let largeText = document.querySelector("#largerLinksBtn");
     largeText.addEventListener("click", increaseTheText);
 
@@ -139,12 +110,3 @@ function theNewList(){
         list.appendChild(unorderedList);
     
 }
-=======
-let bigLinksBtn = document.querySelector("largerLinksBtn");
-
-bigLinksBtn.addEventListener("click",largeFontSize);
-
-function largeFontSize(font){
-    font.style.font-size =  "76px";
-}
->>>>>>> Stashed changes
