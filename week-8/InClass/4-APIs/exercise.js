f8c4bb2165db0778c6caae640fe72666263613d9
Aras Sulaiman <arasko87@gmail.com>
2020-07-04 14:06:57 +0100

    Fetch API DONE

--- week-8/InClass/4-APIs/exercise.js
+++ week-8/InClass/4-APIs/exercise.js
@@ -7,3 +7,20 @@
 
        Remember to open "index.html" using Live Preview, and test that you get the correct results in the console!
  */
+
+fetch("https://cat-fact.herokuapp.com/facts")
+.then(function(result){
+    return result.json();
+})
+.then(function(cat){
+    console.log(cat.all[0].text);
+});
+
+
+fetch("https://restcountries.eu/rest/v2/name/Great%20Britain?fullText=true")
+.then( function (uk){
+   return uk.json();
+})
+.then(function(ukP){
+   console.log(ukP[0].population);
+})
